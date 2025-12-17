45164aa3b225fa3abf608a9ea9e447537f3a212d
bala-amypo <bala@amypo.in>
2025-12-17 09:56:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,4 +6,8 @@ import com.example.demo.service.StudentService;
 @RestController
 public class StudentController{
     @Autowired StudentService ser;
+    @PostMapping("/post")
+    public StudentEntity sendData(@RequestBody StudentEntity stu){
+
+    }
 }
